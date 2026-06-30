8643145c6121b1f657b5c86f143660970b10df31
Bret Pehrson <bretutah@gmail.com>
2026-06-30 14:19:28 -0600

    EF Migration

---
+++ PennySaver.API/Migrations/20260630201843_RemovingIsRevokedAndRenamingTables.cs
@@ -0,0 +1,435 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PennySaver.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovingIsRevokedAndRenamingTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Categories_Users_UserId",
+                table: "Categories");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_RefreshTokens_Users_UserId",
+                table: "RefreshTokens");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_Accounts_AccountId",
+                table: "Transactions");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_Categories_CategoryId",
+                table: "Transactions");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserInfo_Users_UserId",
+                table: "UserInfo");
+
+            migrationBuilder.DropTable(
+                name: "Accounts");
+
+            migrationBuilder.DropTable(
+                name: "Budgets");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Transactions",
+                table: "Transactions");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_RefreshTokens",
+                table: "RefreshTokens");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Categories",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "IsRevoked",
+                table: "RefreshTokens");
+
+            migrationBuilder.RenameTable(
+                name: "Users",
+                newName: "User");
+
+            migrationBuilder.RenameTable(
+                name: "Transactions",
+                newName: "Transaction");
+
+            migrationBuilder.RenameTable(
+                name: "RefreshTokens",
+                newName: "RefreshToken");
+
+            migrationBuilder.RenameTable(
+                name: "Categories",
+                newName: "Category");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Transactions_CategoryId",
+                table: "Transaction",
+                newName: "IX_Transaction_CategoryId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Transactions_AccountId",
+                table: "Transaction",
+                newName: "IX_Transaction_AccountId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_RefreshTokens_UserId",
+                table: "RefreshToken",
+                newName: "IX_RefreshToken_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Categories_UserId",
+                table: "Category",
+                newName: "IX_Category_UserId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_User",
+                table: "User",
+                column: "UserId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Transaction",
+                table: "Transaction",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_RefreshToken",
+                table: "RefreshToken",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Category",
+                table: "Category",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Account",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    AccountName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Account", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Account_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Budget",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TargetAmount = table.Column<double>(type: "float", nullable: false),
+                    Month = table.Column<int>(type: "int", nullable: false),
+                    Year = table.Column<int>(type: "int", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Budget", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Budget_Category_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Category",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Budget_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Account_UserId",
+                table: "Account",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Budget_CategoryId",
+                table: "Budget",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Budget_UserId",
+                table: "Budget",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Category_User_UserId",
+                table: "Category",
+                column: "UserId",
+                principalTable: "User",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RefreshToken_User_UserId",
+                table: "RefreshToken",
+                column: "UserId",
+                principalTable: "User",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transaction_Account_AccountId",
+                table: "Transaction",
+                column: "AccountId",
+                principalTable: "Account",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transaction_Category_CategoryId",
+                table: "Transaction",
+                column: "CategoryId",
+                principalTable: "Category",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserInfo_User_UserId",
+                table: "UserInfo",
+                column: "UserId",
+                principalTable: "User",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Category_User_UserId",
+                table: "Category");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_RefreshToken_User_UserId",
+                table: "RefreshToken");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transaction_Account_AccountId",
+                table: "Transaction");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transaction_Category_CategoryId",
+                table: "Transaction");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserInfo_User_UserId",
+                table: "UserInfo");
+
+            migrationBuilder.DropTable(
+                name: "Account");
+
+            migrationBuilder.DropTable(
+                name: "Budget");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_User",
+                table: "User");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Transaction",
+                table: "Transaction");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_RefreshToken",
+                table: "RefreshToken");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Category",
+                table: "Category");
+
+            migrationBuilder.RenameTable(
+                name: "User",
+                newName: "Users");
+
+            migrationBuilder.RenameTable(
+                name: "Transaction",
+                newName: "Transactions");
+
+            migrationBuilder.RenameTable(
+                name: "RefreshToken",
+                newName: "RefreshTokens");
+
+            migrationBuilder.RenameTable(
+                name: "Category",
+                newName: "Categories");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Transaction_CategoryId",
+                table: "Transactions",
+                newName: "IX_Transactions_CategoryId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Transaction_AccountId",
+                table: "Transactions",
+                newName: "IX_Transactions_AccountId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_RefreshToken_UserId",
+                table: "RefreshTokens",
+                newName: "IX_RefreshTokens_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Category_UserId",
+                table: "Categories",
+                newName: "IX_Categories_UserId");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRevoked",
+                table: "RefreshTokens",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "UserId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Transactions",
+                table: "Transactions",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_RefreshTokens",
+                table: "RefreshTokens",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Categories",
+                table: "Categories",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    AccountName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Accounts_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Budgets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    Month = table.Column<int>(type: "int", nullable: false),
+                    TargetAmount = table.Column<double>(type: "float", nullable: false),
+                    Year = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Budgets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Budgets_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Budgets_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accounts_UserId",
+                table: "Accounts",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Budgets_CategoryId",
+                table: "Budgets",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Budgets_UserId",
+                table: "Budgets",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Categories_Users_UserId",
+                table: "Categories",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RefreshTokens_Users_UserId",
+                table: "RefreshTokens",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_Accounts_AccountId",
+                table: "Transactions",
+                column: "AccountId",
+                principalTable: "Accounts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_Categories_CategoryId",
+                table: "Transactions",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserInfo_Users_UserId",
+                table: "UserInfo",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
